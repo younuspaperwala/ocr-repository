0b5fb6852d749bbb013f1e7de4e18d1ea2753034
Orion Lowy <orionlowy@gmail.com>
2021-11-05 15:20:08 -0500

    Fixes to message.py

--- message.py
+++ message.py
@@ -3,18 +3,27 @@ import json
 
 
 def pack_message(image, bucket, filename):
-    message = {'image': base64.b64encode(image),
+    # Convert the image to a string in base64 format
+    image_str = base64.b64encode(image).decode('ascii')
+
+    # Package the image along with other arguments
+    message = {'image': image_str,
                'bucket': bucket,
                'filename': filename}
 
+    # Convert the complete package into a binary object containing JSON
     return json.dumps(message).encode('utf-8')
 
 
 def unpack_message(event):
-    message_data = base64.b64decode(event["data"]).decode("utf-8")
-    message = json.loads(message_data)
+    # Unpack the binary JSON object
+    message_data = base64.b64decode(event["data"])
+    message = json.loads(message_data.decode("utf-8"))
 
-    image = base64.b64decode(message['image'])
+    image_str = message['image']
+
+    # Get the image from the coded string
+    image = base64.b64decode(image_str.encode('ascii'))
 
     return image, message['bucket'], message['filename']
 
