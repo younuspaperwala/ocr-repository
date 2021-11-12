858c6cc3a2fda618f5ddc1fb13817ceba0bbb4e7
Orion Lowy <orionlowy@gmail.com>
2021-11-12 12:47:36 -0600

    Modify filename new format

--- google/batch.py
+++ google/batch.py
@@ -15,7 +15,7 @@ def select_publish_topic(is_processing_on):
 
 
 def modify_filename(filename, date_time, is_processing_on):
-    return f"{filename[:-4]}.{date_time}_{is_processing_on}"
+    return f"{date_time}_{is_processing_on}/{filename[:-4]}"
 
 
 def load_input(filename):
